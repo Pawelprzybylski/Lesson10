831681433ca2cc1c0303fcc801623c950897aa8a
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-12 13:02:42 +0200

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\pawel\\IdeaProjects\\Lesson10Task4\\src\\main\\text.txt");
+
+        String content = Files.readString(file.toPath());
+
+        boolean findWord = content.contains("Any text");
+
+        System.out.println(findWord);
+    }
+
+}
