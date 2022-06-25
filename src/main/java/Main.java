2b8d23397dc91ea776679468c44cde34a2aba5f3
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-25 11:34:28 +0200

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
 public class Main {
+
     public void validate(int[] values) {
-
         for (int i = 0; i < values.length; i++) {
-            if (i % 3 == 0) {
-                System.out.println("Liczba jest podzielna przez 3");
-            } else {
-                throw new IllegalArgumentException("Liczba jest niepodzielna przez 3");
-            }
-        }
 
+            try {
+                if (values[i] % 3 == 0) {
+                    System.out.println("Liczba jest podzielna przez 3");
+                } else {
+                    throw new IllegalArgumentException("Liczba jest niepodzielna przez 3");
+                }
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+
+        }
     }
+
 }
 
