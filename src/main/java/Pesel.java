6318b2ecb30abd4362460dddea9e3e48ae7d3d8c
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-08 09:28:50 +0200

    Commit

--- src/main/java/Pesel.java
+++ src/main/java/Pesel.java
@@ -3,7 +3,7 @@ public class Pesel {
     public static void validatePesel(String pesel) {
         if (pesel.length() != 11) {
             throw new IllegalLengthException();
-        } else if (pesel.matches(".*\\d.*")) {
+        } else if (!pesel.matches(".*\\d.*")) {
             throw new WrongTypeOfDataException();
         }
     }
